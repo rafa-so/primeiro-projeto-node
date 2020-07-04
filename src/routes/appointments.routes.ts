2d22a1a56325b124cd912746ebac94391cd2d071
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-07-04 00:36:12 -0300

    Add repository and models

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all();
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const fildAppointmentInSameDate = appointmentRepository.findByDate(
+    parsedDate,
+  );
+
+  if (fildAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentRepository.create(provider, date);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>
-  response.json({ message: 'hello world!!!' }),
-);
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Servidor subiu');
